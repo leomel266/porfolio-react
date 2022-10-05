c795ff4a882548394ad86552268c50be06d4ed2b
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2022-10-05 09:57:21 -0300

    link

--- src/components/Slider/Slides.js
+++ src/components/Slider/Slides.js
@@ -5,18 +5,21 @@ import "./Slider.css";
 const slidesInfo = [
   {
     src: Movies,
-    alt: "p1",
+    alt: "Movie search engine",
     desc: "Movie search engine",
+    link: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
   },
   {
     src: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
     alt: "p2",
     desc: "p2",
+    link: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
   },
   {
     src: "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
     alt: "p3",
     desc: "p3",
+    link: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
   },
 ];
 
@@ -25,6 +28,9 @@ const slides = slidesInfo.map((slide) => (
     <img src={slide.src} alt={slide.alt} />
     <div className='slide-desc'>
       <span>{slide.desc}</span>
+      <a href='https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg'>
+        Go to see it
+      </a>
     </div>
   </div>
 ));
